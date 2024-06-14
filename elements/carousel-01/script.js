82b5bd862734dfb2fe8b115d091383173e39c326
~skysfall1ng~ <skythecreative@gmail.com>
2024-06-13 22:38:36 -0700

    rotating artists page

--- elements/carousel-01/script.js
+++ elements/carousel-01/script.js
@@ -1,20 +1,21 @@
-
 const container = document.querySelector(".container");
 const containerCarousel = container.querySelector(".container-carousel");
 const carousel = container.querySelector(".carousel");
 const carouselItems = carousel.querySelectorAll(".carousel-item");
+const descriptionElement = document.getElementById("description");
 
 let isMouseDown = false;
 let currentMousePos = 0;
 let lastMousePos = 0;
 let lastMoveTo = 0;
 let moveTo = 0;
+let closestItemIndex = 0;
 
 const createCarousel = () => {
   const carouselProps = onResize();
   const length = carouselItems.length;
-  const degrees = 360 / length; 
-  const gap = 20; 
+  const degrees = 360 / length;
+  const gap = 45;
   const tz = distanceZ(carouselProps.w, length, gap);
   const height = calculateHeight(tz);
 
@@ -33,7 +34,7 @@ const lerp = (a, b, n) => {
 };
 
 const distanceZ = (widthElement, length, gap) => {
-  return widthElement / 2 / Math.tan(Math.PI / length) + gap; 
+  return widthElement / 2 / Math.tan(Math.PI / length) + gap;
 };
 
 const calculateHeight = (z) => {
@@ -43,31 +44,22 @@ const calculateHeight = (z) => {
 };
 
 const calculateFov = (carouselProps) => {
-  const perspective = window
-    .getComputedStyle(containerCarousel)
-    .perspective.split("px")[0];
-
-  const length =
-    Math.sqrt(carouselProps.w * carouselProps.w) +
-    Math.sqrt(carouselProps.h * carouselProps.h);
-  const fov = 2 * Math.atan(length / 
-    (2 * perspective)) * (180 / Math.PI);
+  const perspective = window.getComputedStyle(containerCarousel).perspective.split("px")[0];
+  const length = Math.sqrt(carouselProps.w * carouselProps.w) + Math.sqrt(carouselProps.h * carouselProps.h);
+  const fov = 2 * Math.atan(length / (2 * perspective)) * (180 / Math.PI);
   return fov;
 };
 
 const getPosX = (x) => {
   currentMousePos = x;
-
-  moveTo = currentMousePos < lastMousePos ? 
-   moveTo - 2 : moveTo + 2;
-
+  moveTo = currentMousePos < lastMousePos ? moveTo - 2 : moveTo + 2;
   lastMousePos = currentMousePos;
 };
 
 const update = () => {
-  lastMoveTo = lerp(moveTo, lastMoveTo, 0.05);
+  lastMoveTo = lerp(moveTo, lastMoveTo, 0.025);
   carousel.style.setProperty("--rotatey", lastMoveTo + "deg");
-
+  updateClosestItem();
   requestAnimationFrame(update);
 };
 
@@ -82,34 +74,95 @@ const onResize = () => {
 
 const initEvents = () => {
   carousel.addEventListener("mousedown", () => {
-    isMouseDown = true; carousel.style.cursor = "grabbing";
+    isMouseDown = true;
+    carousel.style.cursor = "grabbing";
   });
+
   carousel.addEventListener("mouseup", () => {
-    isMouseDown = false; carousel.style.cursor = "grab";
+    isMouseDown = false;
+    carousel.style.cursor = "grab";
   });
+
   carousel.addEventListener("mouseleave", () => {
-    isMouseDown = false
+    if (isMouseDown) {
+      isMouseDown = false;
+    }
+    snapToClosestItem();
+
   });
-  carousel.addEventListener(
-    "mousemove", (e) => isMouseDown && getPosX(e.clientX)
-  );
+  carousel.addEventListener("mousemove", (e) => isMouseDown && getPosX(e.clientX));
   carousel.addEventListener("touchstart", () => {
-    isMouseDown = true; carousel.style.cursor = "grabbing";
+    isMouseDown = true;
+    carousel.style.cursor = "grabbing";
   });
   carousel.addEventListener("touchend", () => {
-    isMouseDown = false; carousel.style.cursor = "grab";
+    isMouseDown = false;
+    carousel.style.cursor = "grab";
+    snapToClosestItem();
   });
-  carousel.addEventListener(
-    "touchmove", (e) => isMouseDown && getPosX(e.touches[0].clientX)
-  );
-
+  carousel.addEventListener("touchmove", (e) => isMouseDown && getPosX(e.touches[0].clientX));
   window.addEventListener("resize", createCarousel);
-
   update();
   createCarousel();
 };
 
+const updateClosestItem = () => {
+  const length = carouselItems.length;
+  const degrees = 360 / length;
+  let currentRotation = lastMoveTo % 360;
+
+  if (currentRotation < 0) {
+    currentRotation += 360;
+  }
+
+  let closestItemAngle = 360;
+  carouselItems.forEach((item, i) => {
+    let itemAngle = degrees * i;
+    itemAngle = itemAngle % 360;
+
+    let diff = Math.abs(currentRotation - itemAngle);
+    if (diff > 180) {
+      diff = 360 - diff;
+    }
+
+    if (diff < closestItemAngle) {
+      closestItemAngle = diff;
+      closestItemIndex = i;
+    }
+  });
+
+  const closestItem = carouselItems[closestItemIndex];
+  const description = closestItem.getAttribute("data-description");
+  descriptionElement.innerText = description;
+
+};
+
+const snapToClosestItem = () => {
+  const length = carouselItems.length;
+  const degrees = 360 / length;
+  const targetAngle = degrees * closestItemIndex;
+  let currentRotation = lastMoveTo % 360;
+
+  // Normalize currentRotation to be within the range [0, 360)
+  if (currentRotation < 0) {
+    currentRotation += 360;
+  }
+
+  // Calculate the shortest angle to rotate to reach the target angle
+  let shortestAngle = targetAngle - currentRotation;
+  if (shortestAngle > 180) {
+    shortestAngle -= 360;
+  } else if (shortestAngle < -180) {
+    shortestAngle += 360;
+  }
+
+  // Calculate the new rotation value
+  moveTo = currentRotation + shortestAngle;
+
+  console.log("snapToClosestItem ->", "currentRotation", currentRotation, "shortestAngle", shortestAngle, "moveTo", moveTo);
+};
+
+
+
 initEvents();
 // inspired by programador.cs
-
-
